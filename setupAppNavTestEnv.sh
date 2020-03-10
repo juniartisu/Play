0025b4535e8ffee1c3947ac33017a935bd4c565b
Juniarti A.Suryakusuma <28314552+juniartisu@users.noreply.github.com>
2020-03-10 16:17:53 -0500

    Update setupAppNavTestEnv.sh

--- setupAppNavTestEnv.sh
+++ setupAppNavTestEnv.sh
@@ -7,13 +7,14 @@ echo $DOCKER_PWD
 echo $PLATFORM_URL
 echo $PLATFORM_USERNAME
 echo $PLATFORM_PASSWORD
+echo $JUNI_VAR
 
 echo $TRAVIS
 echo $TRAVIS_PULL_REQUEST
 echo $TRAVIS_BRANCH
 
 if [ "$TRAVIS" == "true" ] && [ "$TRAVIS_TAG"  == "" ]; then
-     echo "OLA"
+     echo "OLLA"
 fi
 
 echo "GOODBYE JUNI"
