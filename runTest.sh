ebac86864638ce39f57011686847e655314a49d6
juniartisu <jun1art1@gmail.com>
2020-03-12 06:43:31 -0500

    test2

--- runTest.sh
+++ runTest.sh
@@ -4,5 +4,4 @@ echo "Git Tag: $gitTag"
 
 if [ "$TRAVIS" == "true" ] && [ "$gitTag"  != "" ]; then
      echo "Branch has tag, so running tests now"
-     ./setupAppNavTestEnv.sh $OCP_URL $OCP_USER $OCP_PWD OCP $DOCKER_USER
 fi
--- setupAppNavTestEnv.sh
+++ setupAppNavTestEnv.sh
@@ -4,19 +4,11 @@ echo "JUNI start"
 echo $TRAVIS_TAG
 echo "JUNI END"
 
-platformURL=$1
-platformUserName=$2
-platformPassword=$3
+gitTag=`git tag`
+echo "Git Tag: $gitTag"
 
 if [ "$TRAVIS" == "true" ] && [ "$TRAVIS_TAG"  == "" ]; then
      echo "HAS TAG"
-     oc login -u $platformUsername -p $platformPassword $platformURL --insecure-skip-tls-verify
-     if [ $? -eq 0 ]; then
-          echo "########## OC login successful ##########"
-     else
-          echo "########## OC login failed, exiting ##########"
-          exit 1
-     fi
 fi
 
 echo "GOODBYE JUNI"
