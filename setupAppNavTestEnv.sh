587c9957c436bad9f27a5a5a375f6153d6e31252
juniartisu <jun1art1@gmail.com>
2020-03-17 09:37:25 -0500

    test

--- setupAppNavTestEnv.sh
+++ setupAppNavTestEnv.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-
-echo "JUNI start"
-echo $TRAVIS_TAG
-echo "JUNI END"
-
-gitTag=`git tag`
-echo "Git Tag: $gitTag"
-
-echo "Travis tag is $TRAVIS_TAG"
-
-test=`git tag -ln --format '%(subject)' $TRAVIS_TAG`
-echo $TRAVIS_TAG
-echo $test
-
-if [ "$TRAVIS" == "true" ] && [ "$TRAVIS_TAG"  == "" ]; then
-     echo "HAS TAG"
-else
-     echo "DOES NOT HAVE TAG"
-fi
-
+echo "HELLO JUNI"
 echo "GOODBYE JUNI"
