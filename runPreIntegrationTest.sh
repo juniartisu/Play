c882933efdb1a5df5a56929072bf09812267a99c
juniartisu <jun1art1@gmail.com>
2020-03-17 09:33:14 -0500

    email1

--- runPreIntegrationTest.sh
+++ runPreIntegrationTest.sh
@@ -1,27 +1,3 @@
 #!/bin/bash
-export PLATFORM_URL=$OCP_URL
-export PLATFORM_PASSWORD=$OCP_PWD
-export PLATFORM_URL=$OCP_URL
-export PLATFORM_USERNAME=$OCP_USER
-
 echo "HELLO JUNI"
-echo $DOCKER_USER
-echo $DOCKER_PWD
-echo $PLATFORM_URL
-echo $PLATFORM_USERNAME
-echo $PLATFORM_PASSWORD
-
-echo $TRAVIS_BRANCH
-echo $TRAVIS_TAG
-
-gitTag=`git tag`
-echo $gitTag
-
-if [ "$TRAVIS" == "true" ] && [ "$gitTag"  == "" ]; then
-     echo "Branch do not have tag"
-else
-     echo "Branch has tag"
-     ./setupAppNavTestEnv.sh $OCP_URL $OCP_USER $OCP_PWD $DOCKER_USER -p
-fi
-
 echo "GOODBYE JUNI"
